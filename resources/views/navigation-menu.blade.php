3850fbd44656e3f08005da9697d5aa59d716d6a2
Amir <amirhassan.hamzan@gmail.com>
2023-11-05 10:20:36 +0800

    Add FirstPage Controller

--- resources/views/navigation-menu.blade.php
+++ resources/views/navigation-menu.blade.php
@@ -15,6 +15,9 @@
                     <x-nav-link href="{{ route('dashboard') }}" :active="request()->routeIs('dashboard')">
                         {{ __('Dashboard') }}
                     </x-nav-link>
+                    <x-nav-link href="{{ route('firConIndex') }}" :active="request()->routeIs('firConIndex')">
+                        {{ __('First Page') }}
+                    </x-nav-link>
                 </div>
             </div>
 
